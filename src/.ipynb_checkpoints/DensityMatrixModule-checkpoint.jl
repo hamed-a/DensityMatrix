06d7bfd4bf248bb337679538d68897b6cd18214f
hamed <hamed.asasi@gmail.com>
2021-02-03 13:16:20 -0800

    deleted:    src/.ipynb_checkpoints

--- src/.ipynb_checkpoints/DensityMatrixModule-checkpoint.jl
+++
@@ -1,7 +0,0 @@
-module DensityMatrixModule
-include("density_matrix.jl")
-export prod_dims
-export DensityMatrix, DimType, DMConstructMethod, construct_DensityMatrix
-export from_user, from_eigvals, random_rank, random_pure, from_user_pure
-
-end
